9226677d65623748953a997f57583b4f7efd84b1
blandri <landrybrok3@gmail.com>
2022-02-08 10:09:49 +0200

    ft:for testing deployment

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import multer from 'multer';
-import routes from './routes';
-import 'dotenv/config';
-import morgan from 'morgan';
+import routes from './routes/index.js';
 import cors from 'cors';
+import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+import 'dotenv/config';
 
 const app = express();
 
@@ -14,32 +14,40 @@ const mode = process.env.NODE_ENV || 'development';
 
 try {
   if (mode === 'development') {
-    mongoose.connect(process.env.DEVELOPMENT_DB, {
-      useNewUrlParser: true,
-    });
+    mongoose
+      .connect(process.env.DEVELOPMENT_DB, { useNewUrlParser: true })
+      .then((res) => {
+        console.log('DEV DB CONNECTED');
+      });
   } else if (mode === 'test') {
-    mongoose.connect(process.env.TEST_DB, {
-      useNewUrlParser: true,
-    });
+    mongoose
+      .connect(process.env.TEST_DB, { useNewUrlParser: true })
+      .then((res) => {
+        console.log('TEST DB CONNECTED');
+      });
   } else if (mode === 'production') {
-    mongoose.connect(process.env.PRODUCTION_DB, {
-      useNewUrlParser: true,
-    });
+    mongoose
+      .connect(process.env.PRODUCTION_DB, { useNewUrlParser: true })
+      .then((res) => {
+        console.log('PROD DB CONNECTED');
+      });
   }
   app.use(express.json());
   app.use(cors());
   app.use(morgan('dev'));
 
   app.get('/', (req, res) => {
-    res.send({ message: 'welcome to the api' });
-  });
-  app.get('*', (req, res, next) => {
-    res.status(404).json({
-      error: 'Page not found',
-    });
+    res.json({ message: 'Welcome to the API' });
   });
 
   app.use('/api/v1/', routes);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      error: 'NOT FOUND',
+    });
+  });
+
   app.listen(port, () => {
     console.log(`The server is running on port ${port}`);
   });
